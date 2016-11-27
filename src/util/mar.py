42a3bdb161bbb780e4edb5b08886e552d5a82605
Zhe Yu <zyu9@ncsu.edu>
2016-11-27 17:09:23 -0500

    decodeerror

--- src/util/mar.py
+++ src/util/mar.py
@@ -98,7 +98,7 @@ class MAR(object):
 
         ### Feature selection by tfidf in order to keep vocabulary ###
         tfidfer = TfidfVectorizer(lowercase=True, stop_words="english", norm=None, use_idf=True, smooth_idf=False,
-                                sublinear_tf=False)
+                                sublinear_tf=False,decode_error="ignore")
         tfidf = tfidfer.fit_transform(content)
         weight = tfidf.sum(axis=0).tolist()[0]
         kept = np.argsort(weight)[-self.fea_num:]
@@ -107,7 +107,7 @@ class MAR(object):
 
         ### Term frequency as feature, L2 normalization ##########
         tfer = TfidfVectorizer(lowercase=True, stop_words="english", norm=u'l2', use_idf=False,
-                        vocabulary=self.voc)
+                        vocabulary=self.voc,decode_error="ignore")
         self.csr_mat=tfer.fit_transform(content)
         ########################################################
         return
